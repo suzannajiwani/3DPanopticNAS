5a2f625e4d7fb0233bd46a14ae87c64650fd5039
Suzanna <suzannajiwani@gmail.com>
2021-12-03 22:21:33 -0500

    put model in correct file structure, need model conf?

---
+++ torch_points3d/models/panopticseg/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch_points3d
+import matplotlib.pyplot as plt
+import os
+
+from torchvision import models
+from torchvision.models import AlexNet
+
+class Panoster(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.encoder_downsample = self.cfg.MODEL.ENCODER.DOWNSAMPLE
+        self.encoder_out_channels = self.cfg.MODEL.ENCODER.OUT_CHANNELS
+
+        # Encoder
+        self.encoder = torch_points3d.applications.kpconv.KPConv(architecture
+        
+        # Decoders
+        self.semantic_decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 256), nn.ReLU(), nn.Linear(256, 512), nn.ReLU(), 
+                                              nn.Linear(512, 1024), nn.ReLU(), nn.Linear(1024, 2028), nn.ReLU(), nn.Linear(2048, self.cfg.DATASET.MAX_LIDAR_POINTS))
+        self.instance_decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 256), nn.ReLU(), nn.Linear(256, 512), nn.ReLU(), 
+                                              nn.Linear(512, 1024), nn.ReLU(), nn.Linear(1024, 2028), nn.ReLU(), nn.Linear(2048, self.cfg.DATASET.MAX_LIDAR_POINTS))
+
+        # Semantic Head
+#         self.semantic_head = models.resnet101(pretrained=True)
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
+#         # resnet variants:
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True)
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'resnet101', pretrained=True)
+#         # self.semantic_head = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
+        
+#         # Instance Head
+#         # not sure if this is an appropriate head
+#         self.instance_head = models.resnet101(pretrained=True)
+        
+    def forward(self, lidar_input):
+        print("in forward!")
+        output = {}
+
+        print(lidar_input)
+        encoder_out = self.encoder(lidar_input)
+        
+        # semantic
+#         semantic = self.semantic_decoder(encoder_out)
+        
+#         # instance
+#         instance = self.instance_decoder(encoder_out)
+
+        return encoder_out
+    
